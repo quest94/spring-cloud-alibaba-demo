e1bb6cf1b56cecdf97cbf3a7340171f7889db2b0
quest94 <quest94@163.com>
2023-12-06 10:47:51 +0800

    代码块限流工具类优化

--- common/src/main/java/com/quest94/demo/sca/common/util/FlowRegulateUtils.java
+++ common/src/main/java/com/quest94/demo/sca/common/util/FlowRegulateUtils.java
@@ -2,12 +2,13 @@ package com.quest94.demo.sca.common.util;
 
 import com.alibaba.csp.sentinel.Entry;
 import com.alibaba.csp.sentinel.SphU;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.quest94.demo.sca.common.exception.FlowRegulateException;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class FlowRegulateUtils {
@@ -25,11 +26,112 @@ public class FlowRegulateUtils {
         FlowRuleManager.loadRules(Arrays.asList(rules));
     }
 
-    public static <T> T runInFlowRegulate(String resourceName, Supplier<T> supplier) {
+    public static <T> T within(String resourceName, Supplier<T> supplier) {
+        return within(resourceName, supplier, null, null);
+    }
+
+    public static <T> T within(String resourceName, Supplier<T> supplier,
+                               Function<BlockException, T> blockHandler,
+                               Function<Throwable, T> fallbackHandler) {
         try (Entry entry = SphU.entry(resourceName)) {
             return supplier.get();
-        } catch (BlockException e) {
+        } catch (com.alibaba.csp.sentinel.slots.block.BlockException e) {
+            if (Objects.nonNull(blockHandler)) {
+                return blockHandler.apply(new BlockException(e));
+            }
             throw new FlowRegulateException(resourceName);
+        } catch (Throwable e) {
+            if (Objects.nonNull(blockHandler)) {
+                return fallbackHandler.apply(e);
+            }
+            throw e;
+        }
+    }
+
+    public static class BlockException extends com.alibaba.csp.sentinel.slots.block.BlockException {
+        private final com.alibaba.csp.sentinel.slots.block.BlockException cause;
+
+        public BlockException(com.alibaba.csp.sentinel.slots.block.BlockException cause) {
+            super(cause.getMessage(), cause);
+            this.cause = cause;
+        }
+
+        @Override
+        public Throwable fillInStackTrace() {
+            return cause.fillInStackTrace();
+        }
+
+        @Override
+        public String getRuleLimitApp() {
+            return cause.getRuleLimitApp();
+        }
+
+        @Override
+        public void setRuleLimitApp(String ruleLimitApp) {
+            cause.setRuleLimitApp(ruleLimitApp);
+        }
+
+        @Override
+        public RuntimeException toRuntimeException() {
+            return cause.toRuntimeException();
+        }
+
+        @Override
+        public AbstractRule getRule() {
+            return new AbstractRule(cause.getRule());
+        }
+    }
+
+    public static class AbstractRule extends com.alibaba.csp.sentinel.slots.block.AbstractRule {
+        private final com.alibaba.csp.sentinel.slots.block.AbstractRule rule;
+
+        public AbstractRule(com.alibaba.csp.sentinel.slots.block.AbstractRule rule) {
+            this.rule = rule;
+        }
+
+        @Override
+        public Long getId() {
+            return rule.getId();
+        }
+
+        @Override
+        public com.alibaba.csp.sentinel.slots.block.AbstractRule setId(Long id) {
+            return rule.setId(id);
+        }
+
+        @Override
+        public String getResource() {
+            return rule.getResource();
+        }
+
+        @Override
+        public com.alibaba.csp.sentinel.slots.block.AbstractRule setResource(String resource) {
+            return rule.setResource(resource);
+        }
+
+        @Override
+        public String getLimitApp() {
+            return rule.getLimitApp();
+        }
+
+        @Override
+        public com.alibaba.csp.sentinel.slots.block.AbstractRule setLimitApp(String limitApp) {
+            return rule.setLimitApp(limitApp);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return rule.equals(o);
+        }
+
+        @Override
+        public <T extends com.alibaba.csp.sentinel.slots.block.AbstractRule> T as(Class<T> clazz) {
+            return rule.as(clazz);
+        }
+
+        @Override
+        public int hashCode() {
+            return rule.hashCode();
         }
     }
 
